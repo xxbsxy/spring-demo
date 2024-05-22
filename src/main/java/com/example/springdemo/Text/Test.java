c2f68f91ff8aecaad8afe11411f77321dbad6242
陈桂东 <2873336572@qq.com>
2024-05-22 16:00:53 +0800

    commit

--- src/main/java/com/example/springdemo/Text/Test.java
+++
@@ -1,7 +0,0 @@
-package com.example.springdemo.Text;
-
-import com.example.springdemo.Annotation.TestAnnotation;
-
-@TestAnnotation()
-public class Test {
-}
---
+++ src/main/java/com/example/springdemo/text/Main.java
@@ -0,0 +1,8 @@
+package com.example.springdemo.text;
+
+public class Main {
+    public static void main(String[] args) {
+        Test t = new Test();
+        System.out.println(t);
+    }
+}
