c1760e5d944607db0b06a38b8c49aa7abcf69cc8
陈桂东 <2873336572@qq.com>
2024-05-23 17:01:12 +0800

    commit

--- spring-api/src/main/java/com/cgdcgd/cc/service/impl/StudentServiceImpl.java
+++ spring-api/src/main/java/com/cgdcgd/cc/service/impl/StudentServiceImpl.java
@@ -4,6 +4,7 @@ import com.cgdcgd.cc.entity.Student;
 import com.cgdcgd.cc.result.Result;
 import com.cgdcgd.cc.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.cgdcgd.cc.mapper.StudentMapper;
@@ -14,10 +15,12 @@ import java.util.List;
 public class StudentServiceImpl extends ServiceImpl<StudentMapper, Student> implements StudentService {
     @Autowired
     StudentMapper studentMapper;
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
     @Override
     public Result<List<Student>>  getStudent(String sName, String sSex) {
         List<Student> student =  studentMapper.getStudent(sName, sSex);
-
+        stringRedisTemplate.opsForValue().set("k2","v2");
         return Result.success(student);
     }
 }
